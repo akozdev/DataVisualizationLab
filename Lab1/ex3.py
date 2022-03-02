e88056bcbf00190f89de59eb8dee5a4dd8a5daf6
akozdev <akoz@hey.com>
2022-03-02 17:53:51 +0100

    Add solution to ex3

---
+++ Lab1/ex3.py
@@ -0,0 +1,12 @@
+# Zad3. Napisz skrypt, w którym stworzysz operatory przyrostkowe dla operacji: +, -, *, /, **, %
+
+num = 1
+
+num += 2
+num -= 1
+num *= 10
+num /= 2
+num **= 2
+num %= 3
+
+print(num)

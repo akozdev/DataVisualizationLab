d880cb22281b7c53c438f29d584d30f78be5b5ef
akozdev <akoz@hey.com>
2022-03-02 17:50:56 +0100

    Add solution to ex2

---
+++ Lab1/ex2.py
@@ -0,0 +1,8 @@
+# Zad2. Stwórz skrypt kalkulator, w którym wykorzystać wszystkie podstawowe działania arytmetyczne
+
+print(f'The result of 5 + 5 is {5 + 5}')
+print(f'The result of 10 - 5 is {10 - 5}')
+print(f'The result of 10 * 10 is {10 * 10}')
+print(f'The result of 10 / 5 is {10 / 5}')
+print(f'The result of 10 % 3 is {10 % 3}')
+print(f'The result of 10^2 is {pow(10, 2)}')

9c6fb43054840ca0a0f0c6a15d3e3d978d1cfb7b
akozdev <akoz@hey.com>
2022-03-02 19:30:28 +0100

    Add solution to Lab2/ex1

---
+++ Lab2/ex1.py
@@ -0,0 +1,10 @@
+sport_disciplines = ['Fencing', 'Football', 'Skiing']
+
+sport_disciplines_reversed = []
+
+for sport in reversed(sport_disciplines):
+    sport_disciplines_reversed.append(sport)
+
+sport_disciplines_reversed.append('Swimming')
+
+print(sport_disciplines_reversed)
